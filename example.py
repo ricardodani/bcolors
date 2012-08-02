8b56f8ed602cde6151044e1c0f3a9e69509406f7
Ricardo L. Dani <ricardodani@gmail.com>
2012-08-02 12:15:39 -0300

    Adicionado exemplo

---
+++ example.py
@@ -0,0 +1,7 @@
+from colors import bcolors
+
+print bcolors.header('Header message!')
+print bcolors.okblue('Okblue message!')
+print bcolors.okgreen('Okgreen message!')
+print bcolors.warning('Okwarning message!')
+print bcolors.fail('Fail message!')
