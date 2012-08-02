cf9bfd2d31a18606aef50e87eea0abd4b0bdc42f
Ricardo L. Dani <ricardodani@gmail.com>
2012-08-02 12:13:47 -0300

    Adicionado módulo colors novamente (agora o arquivo real).

---
+++ colors.py
@@ -0,0 +1,35 @@
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
+    @staticmethod
+    def header(msg):
+        return bcolors.HEADER + msg + bcolors.ENDC
+    
+    @staticmethod
+    def okblue(msg):
+        return bcolors.OKBLUE + msg + bcolors.ENDC
+    
+    @staticmethod
+    def okgreen(msg):
+        return bcolors.OKGREEN + msg + bcolors.ENDC
+    
+    @staticmethod
+    def warning(msg):
+        return bcolors.WARNING + msg + bcolors.ENDC
+    
+    @staticmethod
+    def fail(msg):
+        return bcolors.FAIL + msg + bcolors.ENDC
